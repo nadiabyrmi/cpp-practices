5936902d1c5c95c127b2413d435684419535e515
nadia byrmi <nadia.byrmi@gmail.com>
2026-09-21 02:43:08 +0330

    Add project Hello3

---
+++ Hello 3/Hello 3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//testing how comments work in c++, for example this one is short
+	//but if i want to make it 2line, it won't
+	/*but in this version. it will continue to the second line
+	even if i use enter for countles times.*/
+	cout << "Hello, " << "my" << " programmer!" << "\n" << endl;
+	return 0;
+}
