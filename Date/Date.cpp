d6d9e101d947f565b35c3360405b418c3f779428
nadia byrmi <nadia.byrmi@gmail.com>
2026-09-21 13:49:42 +0330

    Add Date ptoject

---
+++ Date/Date.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//this on is a test for printing date
+	cout << "Today Is" << ": " << 21 << " September" << " 2026" << endl;
+	return 0;
+}
