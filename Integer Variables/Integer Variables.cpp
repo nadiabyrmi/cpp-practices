1a6162af9d5418d107a5a8e0bbbb5ea775a13b03
nadia byrmi <nadia.byrmi@gmail.com>
2026-09-21 15:47:10 +0330

    Add Integer variables project

---
+++ Integer Variables/Integer Variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//testing how initializing of variables work & trying to test declaring them in different ways.
+	/*declaring with initializing*/
+	int a = 1;
+	int b = 2;
+	int c, d = 4;
+	int e, f, g;
+	int k,n = 32;
+	c = a + b;
+	e = d + 1;
+	f = e + 1;
+	g = f + 1;
+	cout << "a=" << a << ',' << "b=" << b << ',' << "c=" << c << ',' << "d=" << d << ',' << "e=" << e << ',' << "f=" << f << ',' << "g=" << g << endl;
+	return 0;
+}
