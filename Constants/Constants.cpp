d8f33b06cd53cbcf012324b503c00f9676ab7e85
nadia byrmi <nadia.byrmi@gmail.com>
2026-09-21 16:17:19 +0330

    Add Constanst project

---
+++ Constants/Constants.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	// just playing with constants
+	const string name = "Nadia bayrami";
+	const char letter = 'a';
+	const int number = 9330829610;
+	const float height = 173.5;
+	const bool characteristic = true;
+	const double pi = 3.14159265358979323856;
+	cout << "name is: " << name << " ,letter is: " << letter << " ,number is: " << number << " ,height is:" << height << " ,characterestic is: " << characteristic << " ,pi is: " << pi << endl;
+	return 0;
+}
+
