446fbbe4a46e4f56d799af5dae5f131aa61559da
nadia byrmi <nadia.byrmi@gmail.com>
2026-09-21 13:38:22 +0330

    Add Hello4 project

---
+++ Hello 4/Hello 4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	/*this on is a test for literal & character usage.
+	just extending the line*_*/
+	cout << "Hell" << 'o' << ',' << " my" << ' ' << "programmer" << '!' << '\n' << endl;
+	cout << 1994 << ".10." << '1' << '\n';
+		return 0;
+	}
