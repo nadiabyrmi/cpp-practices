cc99344ce8cf3dc79eaf76f236b104b324896120
nadia byrmi <nadia.byrmi@gmail.com>
2026-09-21 15:59:50 +0330

    Add Not Initialized Variable project

---
+++ Not Initialized Variable/Not Initialized Variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	// testing how will it be seems if i don't initialized a declared varibale
+	int x = 0;
+	int y = 1994;
+	cout << "x is equal to=" << x << ", and y is equal to=" << y <<'.' << endl;
+	return 0;
+	/*so i've tried but compiler didn't even let me to build the program. guess it won't work on advanced ides*/
+}
