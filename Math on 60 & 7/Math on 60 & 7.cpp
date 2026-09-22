5a1ada4fae083e05033d2a7624d6a66df569084e
nadia byrmi <nadia.byrmi@gmail.com>
2026-09-22 23:42:49 +0330

    Add Math on 60 & 7 project

---
+++ Math on 60 & 7/Math on 60 & 7.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int m = 60, n = 7;
+	cout << "numbers we're working on are: 60 & 7" << endl;
+	cout << "60+7= " << m + n << ", " << "60-7= " << m - n << ", " << "60*7= " << m * n << ", " << "60/7= " << m / n << ", " << "m%n= " << m % n << " \n";
+	return 0;
+}
