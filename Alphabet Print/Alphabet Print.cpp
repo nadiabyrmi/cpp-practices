c021060924b459b3efec0761caa1b82d5d7eff67
nadia byrmi <nadia.byrmi@gmail.com>
2026-09-22 15:09:49 +0330

    Add Alphabet Print project

---
+++ Alphabet Print/Alphabet Print.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//trying to print english alphabet 
+	cout << "capital letters\tsmall letters\n";
+	cout << "A\ta\nB\tb\nC\tc\nD\td\nE\te\n.\t.\n.\t.\n.\t.\nX\tx\nY\ty\nZ\tz\n";
+	return 0;
+}
