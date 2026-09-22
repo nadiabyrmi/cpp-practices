f5e94ca5221d8f413a8787f3f9bed3056297e218
nadia byrmi <nadia.byrmi@gmail.com>
2026-09-22 15:22:53 +0330

    Add Letter With Stars project

---
+++ Letter With Stars/Letter With Stars.cpp
@@ -0,0 +1,8 @@
+#include  <iostream>
+using namespace std;
+int main()
+{
+	/*trying to print letter B with stars in 6*7 blocks */
+	cout << "*****\n*    *\n*    *\n*****\n*    *\n*    *\n*****\n";
+	return 0;
+}
