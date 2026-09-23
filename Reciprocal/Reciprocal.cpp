821483d63b0ee2ef382056d74945f1ebc9f7552a
nadia byrmi <nadia.byrmi@gmail.com>
2026-09-23 21:50:13 +0330

    Add Reciprocal project

---
+++ Reciprocal/Reciprocal.cpp
@@ -0,0 +1,10 @@
+#include  <iostream>
+using namespace std;
+int main()
+{
+	double x;
+	cout << "enter a float number: " << endl;
+	cin >> x;
+	cout << "your numberrs reciprocal is: " << 1 / x << endl;
+	return 0;
+}
