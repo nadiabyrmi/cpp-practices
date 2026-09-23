d07c7c7bf5af2e25606a58d2e0156dba07cf9894
nadia byrmi <nadia.byrmi@gmail.com>
2026-09-23 21:58:37 +0330

    Add Boolean Variable project

---
+++ Boolean variable/Boolean variable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	// finding out how a boolean variable work
+	bool sign = false;
+	cout << "sign is= " << sign << endl;
+	sign = true;
+	cout << "sign is= " << sign << endl;
+	return 0;
+}
