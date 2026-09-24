27c9a7a234d5195c3bcd83eff620886f8e7d1fa4
nadia byrmi <nadia.byrmi@gmail.com>
2026-09-24 13:36:50 +0330

    Add Type Casting project

---
+++ Type Casting/Type Casting.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	long double pi = 3.14151617181920;
+	int x;
+	x = int(pi);
+	cout << " number pi is = " << pi << " number x is = " << x << endl;
+	return 0;
+
+}
