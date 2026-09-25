e53324578c036123bffefb7af7b5142e7d92c6a8
nadia byrmi <nadia.byrmi@gmail.com>
2026-09-25 19:17:38 +0330

    Add Minus project

---
+++ Minus/Minus.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 20;
+	cout << " Your Integer Number Is : 20 " << endl;
+	n--;
+	cout << " now n is: " << n << endl;
+	--n;
+	cout << " now n is: " << n << endl;
+	n -= 1;
+	cout << " now n is: " << n << endl;
+	cout << " now n is: " << n - 1 << endl;
+	return 0;
+}
