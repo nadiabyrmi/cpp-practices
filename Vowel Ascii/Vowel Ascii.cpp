08f65c270c6be5f94740f3f2a8cf6bdd1437dd3e
nadia byrmi <nadia.byrmi@gmail.com>
2026-09-25 21:52:52 +0330

    Add Vowel Ascii project

---
+++ Vowel Ascii/Vowel Ascii.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char a1 = 'a', a2 = 'A', e1 = 'e', e2 = 'E', i1 = 'i', i2 = 'I', o1 = 'o', o2 = 'O', u1 = 'u', u2 = 'U';
+
+	cout << "a = " << int(a1) << "\t\tA = " << int(a2);
+	cout << "\ne = " << int(e1) << "\t\tE = " << int(e2);
+	cout << "\ni = " << int(i1) << "\t\tI = " << int(i2);
+	cout << "\no = " << int(o1) << "\t\tO = " << int(o2);
+	cout<< "\nu = " << int(u1) << "\t\tU = " << int(u2);
+	return 0;
+}
